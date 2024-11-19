c2ef083cff376f884dcb15907c459fb95b188a42
Igor Artus <igorengenheirodesoftware@gmail.com>
2024-11-19 17:33:35 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  // Adicione o basePath e assetPrefix se o repositório não for o principal (opcional)
-  basePath: '/studentsTable',
-  assetPrefix: '/studentsTable/',
 };
 
 export default nextConfig;
